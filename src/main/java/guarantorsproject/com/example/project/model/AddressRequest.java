6792a7f2c59b0ef8e9ccdb56de6f31c4f90f16c9
aditya surabhi <aditya.surabhi9@gmail.com>
2025-07-14 23:43:57 -0500

    project test assesment

---
+++ src/main/java/guarantorsproject/com/example/project/model/AddressRequest.java
@@ -0,0 +1,13 @@
+package guarantorsproject.com.example.project.model;
+
+public class AddressRequest {
+    private String inputAddress;
+
+    public String getInputAddress() {
+        return inputAddress;
+    }
+
+    public void setInputAddress(String inputAddress) {
+        this.inputAddress = inputAddress;
+    }
+}
