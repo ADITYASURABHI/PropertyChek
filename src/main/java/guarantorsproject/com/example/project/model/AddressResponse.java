a09cb67c1e97760c6ac8b1f6fdbec75dac6f1003
aditya surabhi <aditya.surabhi9@gmail.com>
2025-07-14 23:43:57 -0500

    project test assesment

---
+++ src/main/java/guarantorsproject/com/example/project/model/AddressResponse.java
@@ -0,0 +1,29 @@
+package guarantorsproject.com.example.project.model;
+
+public class AddressResponse {
+    private String street;
+    private String number;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String status;
+
+    // Getters and Setters
+    public String getStreet() { return street; }
+    public void setStreet(String street) { this.street = street; }
+
+    public String getNumber() { return number; }
+    public void setNumber(String number) { this.number = number; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
+    public String getZipCode() { return zipCode; }
+    public void setZipCode(String zipCode) { this.zipCode = zipCode; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
